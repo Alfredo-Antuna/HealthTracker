8de88dbe4dd38b2bf6a7409454b0db5b72f497a4
Alfredo Antuna <Alfredo.Antuna@lntinfotech.com>
2021-09-27 12:12:54 -0500

    finnished sql commands added CLI switch statements

--- Console/Program.cs
+++ Console/Program.cs
@@ -6,9 +6,53 @@ namespace Console
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            Sql.addDay("2021-09-27",1,1,1);
-            //String date = Console.ReadLine();
+
+            var consoleControl = true;
+            
+            while(consoleControl){
+                Sql.showHealth();
+                Console.WriteLine(@"
+                                    
+                                    Input 0 to Add a Day option 
+                                    Input 1 to Pick a day to Update 
+                                    Input 2 to Pick a day to Delete
+                                    Press Enter to Quit");
+
+
+                var option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "0":
+                        Console.WriteLine(@"Date format YYYY-MM-DD
+                                            Input : Date,BMI,Weight,Calories");
+                        var info = Console.ReadLine();
+                        info = info.split(",");
+                        Sql.addDay(info[0],Convert.ToDecimal(info[1]),Convert.ToDecimal(info[2]),Convert.ToInt32(info[3]));
+                        break;
+                    case "1":
+                    Console.WriteLine(@"Date format YYYY-MM-DD
+                                            Input : Date,Weight,BMI,Calories");
+                        var info = Console.ReadLine();
+                        info = info.split(",");
+                        Sql.updateDay(info[0],Convert.ToDecimal(info[1]),Convert.ToDecimal(info[2]),Convert.ToInt32(info[3]));
+                        break;
+                    case "2":
+                        Console.WriteLine(@"Date format YYYY-MM-DD
+                                            Input : Date");
+                        var info = Console.ReadLine();
+                        Sql.removeDay(info);
+                        break;
+                    case "":
+                        break;
+
+                }
+
+                
+
+
+
+            }
         }
     }
 }
--- Library/Sql.cs
+++ Library/Sql.cs
@@ -25,6 +25,25 @@ namespace Library
             command.Parameters.AddWithValue("$calories",calories);
             using var reader =  command.ExecuteReader();
         }
+        public static void updateDay(string day,decimal bmi,decimal weight,int calories)
+        {  
+            using var connection = new SqliteConnection("Data Source=./Data/health.db");
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                UPDATE healthInfo
+                SET  weight = $weight, bmi = $bmi, calories = $calories)
+                WHERE date = $date
+               
+            ";
+            //YYYY-MM-DD
+            command.Parameters.AddWithValue("$date",day);
+            command.Parameters.AddWithValue("$weight",weight);
+            command.Parameters.AddWithValue("$bmi",bmi);
+            command.Parameters.AddWithValue("$calories",calories);
+            using var reader =  command.ExecuteReader();
+        }
 
 
 
@@ -41,6 +60,31 @@ namespace Library
                 WHERE date = $date;
             ";
             command.Parameters.AddWithValue("$date",day);
+            using var reader = command.ExecuteReader();
+        }
+        public static void showHealth()
+        {
+            using var connection = new SqliteConnection("Data Source=./Data/health.db");
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                SELECT *
+                FROM healthInfo
+            ";
+            using var reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                {
+                var date = reader.GetString(0);
+                var weight = reader.GetDecimal(1);
+                var bmi = reader.GetDecimal(2);
+                var calories = reader.GetInt32(3);
+
+
+                Console.WriteLine($"Row: {date} - {weight} - {bmi} - {calories}");
+            }
+            }
         }
     }
 }
